c7294bcb93545fb9fdafb480f6a2cd5e0f633546
Adrian Chapa <achapa@adrianchapa.dev>
2023-03-21 01:35:35 -0500

    Saving Photos

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -13,6 +13,24 @@ import { Capacitor } from "@capacitor/core"
 
 export function usePhotoGallery() {
 	const [photos, setPhotos] = useState<UserPhoto[]>([])
+	const savePicture = async (
+		photo: Photo,
+		fileName: string
+	): Promise<UserPhoto> => {
+		const base64Data = await base64FromPath(photo.webPath!)
+		const savedFile = await Filesystem.writeFile({
+			path: fileName,
+			data: base64Data,
+			directory: Directory.Data,
+		})
+
+		// Use webPath to display the new image instead of base64 since it's
+		// already loaded into memory
+		return {
+			filepath: fileName,
+			webviewPath: photo.webPath,
+		}
+	}
 	const takePhoto = async () => {
 		const photo = await Camera.getPhoto({
 			resultType: CameraResultType.Uri,
@@ -20,19 +38,31 @@ export function usePhotoGallery() {
 			quality: 100,
 		})
 		const fileName = new Date().getTime() + ".jpeg"
-		const newPhotos = [
-			{
-				filepath: fileName,
-				webviewPath: photo.webPath,
-			},
-			...photos,
-		]
+		const savedFileImage = await savePicture(photo, fileName)
+		const newPhotos = [savedFileImage, ...photos]
 		setPhotos(newPhotos)
 	}
 	return {
 		takePhoto,
 		photos,
 	}
+}
+
+export async function base64FromPath(path: string): Promise<string> {
+	const response = await fetch(path)
+	const blob = await response.blob()
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader()
+		reader.onerror = reject
+		reader.onload = () => {
+			if (typeof reader.result === "string") {
+				resolve(reader.result)
+			} else {
+				reject("method did not return a string")
+			}
+		}
+		reader.readAsDataURL(blob)
+	})
 }
 
 export interface UserPhoto {
